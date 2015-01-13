36afd703c0ae4957c13f99069b744ee958033c80
Bertrand Fan <bertrand@fan.net>
2015-01-12 20:45:26 -0800

    Link to the registry

--- _navigation.php
+++ _navigation.php
@@ -9,5 +9,6 @@
 	<li <?php if ($page === 'ourstory'): ?>class="active"<?php endif; ?>><a href="/ourstory/">Our Story</a></li>
 	<li <?php if ($page === 'weddingweekend'): ?>class="active"<?php endif; ?>><a href="/weddingweekend/">Wedding Weekend</a></li>
 	<li <?php if ($page === 'islainfo'): ?>class="active"<?php endif; ?>><a href="/islainfo/">Isla Info</a></li>
+	<li><a href="http://www.amazon.com/registry/wedding/3FZT3RVMAX0Q1">Registry</a></li>
 	<li <?php if ($page === 'rsvp'): ?>class="active"<?php endif; ?>><a href="/rsvp/">RSVP</a></li>
 </ul>
